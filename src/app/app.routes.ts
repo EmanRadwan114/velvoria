50ff615a2f6eefb08c263a70342d71a89f199686
Eman Radwan <emanalaa11499@gmail.com>
2025-04-28 23:11:00 +0300

    initial project cnfiguration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,55 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
+import { SearchComponent } from './components/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, title: 'Home' },
+  { path: 'categories', component: CategoriesComponent, title: 'Categories' },
+  { path: 'products', component: ProductsComponent, title: 'Products' },
+  { path: 'cart', component: CartComponent, title: 'Cart' },
+  { path: 'search', component: SearchComponent, title: 'Search Results' },
+  {
+    path: 'wishlist',
+    loadComponent: () =>
+      import('./components/wishlist/wishlist.component').then(
+        (c) => c.WishlistComponent
+      ),
+    title: 'Wishlist',
+  },
+  {
+    path: 'profile',
+    loadComponent: () =>
+      import('./components/profile/profile.component').then(
+        (c) => c.ProfileComponent
+      ),
+    title: 'Wishlist',
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./components/about/about.component').then(
+        (c) => c.AboutComponent
+      ),
+    title: 'About',
+  },
+  {
+    path: 'contact-us',
+    loadComponent: () =>
+      import('./components/contacts/contacts.component').then(
+        (c) => c.ContactsComponent
+      ),
+    title: 'Contact Us',
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./components/not-found/not-found.component').then(
+        (c) => c.NotFoundComponent
+      ),
+    title: 'Not Found 404',
+  },
+];
---
+++ src/app/components/wishlist/wishlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wishlist',
+  imports: [],
+  templateUrl: './wishlist.component.html',
+  styles: ``
+})
+export class WishlistComponent {
+
+}
