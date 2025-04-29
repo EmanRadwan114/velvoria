97c40dcabe6a9f516225db1f97a9aa9afee3bd74
Hager Amr <hageramr320@gmail.com>
2025-04-29 22:20:52 +0200

    make navbar and footer responsive

--- src/app/components/sharedComponents/navbar/navbar.component.ts
+++ src/app/components/sharedComponents/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive,CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  menuOpen = false;
+}
