5dc62fdb08b18e93301b776ce101802f21b453e2
Hager Amr <hageramr320@gmail.com>
2025-04-29 21:47:02 +0200

    completed 90% of footer and navbar, only simple hover styling left

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from "./components/sharedComponents/navbar/navbar.component";
+import { FooterComponent } from "./components/sharedComponents/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
