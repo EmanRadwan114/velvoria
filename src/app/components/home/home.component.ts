43e9d8f4d3d709116e2bd396aa3fd5c6f17610a0
Doooaa <d98744330@gmail.com>
2025-05-05 02:48:40 +0300

    add good vibes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { DOTDComponent } from '../dotd/dotd.component';
+import { GoodVibesComponent } from "../good-vibes/good-vibes.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, DOTDComponent],
+  imports: [HeroComponent, DOTDComponent, GoodVibesComponent],
   templateUrl: './home.component.html',
   styles: ``,
 })
