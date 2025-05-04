2c8ace00b5aa48d0e509c87f440452f5a43eced6
Nouran-Alaa <nouranalaa81@gmail.com>
2025-05-04 06:08:56 +0300

    Adding profile

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 import { DOTDComponent } from '../dotd/dotd.component';
+import { ProfileComponent } from '../profile/profile.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, DOTDComponent],
+  imports: [HeroComponent, DOTDComponent, ProfileComponent],
   templateUrl: './home.component.html',
   styles: ``,
 })
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './profile.component.html',
-  styles: ``
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  image = 'https://picsum.photos/350';
 
+  fullName = localStorage.getItem('fullName') || '';
+  email = localStorage.getItem('email') || '';
+  password = localStorage.getItem('password') || '';
+  newPassword = localStorage.getItem('newPassword') || '';
+  bio = localStorage.getItem('bio') || '';
+  country = localStorage.getItem('country') || '';
+  city = localStorage.getItem('city') || '';
+  postalCode = localStorage.getItem('postalCode') || '';
+
+  isEditingPersonalInfo = false;
+  isEditingAddress = false;
+  isPersonalInfo = true;
+  isOrder = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  savePersonalInfo() {
+    localStorage.setItem('fullName', this.fullName);
+    localStorage.setItem('email', this.email);
+    localStorage.setItem('password', this.password);
+    localStorage.setItem('newPassword', this.newPassword);
+    localStorage.setItem('bio', this.bio);
+    this.isEditingPersonalInfo = false;
+  }
+
+  saveAddress() {
+    localStorage.setItem('country', this.country);
+    localStorage.setItem('city', this.city);
+    localStorage.setItem('postalCode', this.postalCode);
+    this.isEditingAddress = false;
+  }
 }
