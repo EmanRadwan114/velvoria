96c5ee2889407605c485305165239ebc079c17d6
Hager Amr <hageramr320@gmail.com>
2025-05-05 03:25:41 +0200

    complete Categories section in home

--- src/app/components/shop-by-categories/shop-by-categories.component.ts
+++ src/app/components/shop-by-categories/shop-by-categories.component.ts
@@ -1,21 +1,39 @@
+import { RouterLink } from '@angular/router';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-shop-by-categories',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './shop-by-categories.component.html',
   styleUrls: ['./shop-by-categories.component.css'],
 })
 export class ShopByCategoriesComponent {
+  //@ViewChild() is a decorator that gives you direct access to a DOM element
   @ViewChild('slider', { static: false }) slider!: ElementRef;
 
   scrollLeft() {
-    this.slider.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
+    const sliderEl = this.slider.nativeElement;
+
+    if (sliderEl.scrollLeft <= 0) {
+      // Jump to the end
+      const maxScrollLeft = sliderEl.scrollWidth - sliderEl.clientWidth;
+      sliderEl.scrollTo({ left: maxScrollLeft, behavior: 'smooth' });
+    } else {
+      sliderEl.scrollBy({ left: -300, behavior: 'smooth' });
+    }
   }
 
   scrollRight() {
-    this.slider.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+    const sliderEl = this.slider.nativeElement;
+    const maxScrollLeft = sliderEl.scrollWidth - sliderEl.clientWidth;
+
+    if (sliderEl.scrollLeft >= maxScrollLeft) {
+      // If we're at the end, go back to start
+      sliderEl.scrollTo({ left: 0, behavior: 'smooth' });
+    } else {
+      sliderEl.scrollBy({ left: 300, behavior: 'smooth' });
+    }
   }
 }
