479c3bbfa2a4703324da2e88d068b3f4a81d9d98
Hager Amr <hageramr320@gmail.com>
2025-05-12 23:47:54 +0200

    complete 50 % of products dashboard

--- src/app/components/dashboard/products-dashboard/products-dashboard.component.ts
+++ src/app/components/dashboard/products-dashboard/products-dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../../../services/products.service';
 import { CommonModule } from '@angular/common';
+import { CategoriesService } from '../../../../services/categories.service';
 
 @Component({
   selector: 'app-products-dashboard',
@@ -10,16 +11,49 @@ import { CommonModule } from '@angular/common';
 })
 export class ProductsDashboardComponent implements OnInit {
   private readonly _ProductsServices = inject(ProductsService);
+  private readonly _CategoriesService = inject(CategoriesService);
   productsList: any[] = [];
+  categories: any[] = [];
 
   ngOnInit(): void {
+    this._CategoriesService.getAllCategories().subscribe({
+      next: (categoryRes: any) => {
+        this.categories = categoryRes.data;
+
+        // Now fetch products after categories are ready
+        this.fetchAllProducts();
+      },
+      error: (err) => {
+        console.log('Failed to load categories', err);
+      },
+    });
     this.fetchAllProducts();
   }
+  // fetchAllProducts() {
+  //   this._ProductsServices.getAllProducts().subscribe({
+  //     next: (res: any) => {
+  //       this.productsList = res.data;
+  //       console.log(res.data);
+  //     },
+  //     error: (err) => {
+  //       console.log(err);
+  //     },
+  //   });
+  // }
   fetchAllProducts() {
     this._ProductsServices.getAllProducts().subscribe({
       next: (res: any) => {
-        this.productsList = res.data;
-        console.log(res.data);
+        this.productsList = res.data.map((product: any) => {
+          const category = this.categories.find(
+            (cat) => cat._id === product.categoryID
+          );
+          return {
+            ...product,
+            categoryName: category?.name || 'Unknown',
+          };
+        });
+
+        console.log(this.productsList);
       },
       error: (err) => {
         console.log(err);
