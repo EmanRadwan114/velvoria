2bb45e1c10305f668cd630fb91daa40986b78383
Hager Amr <hageramr320@gmail.com>
2025-05-12 23:17:11 +0200

    start with products dashboard

--- src/app/components/dashboard/products-dashboard/products-dashboard.component.ts
+++ src/app/components/dashboard/products-dashboard/products-dashboard.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsService } from '../../../../services/products.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products-dashboard.component.html',
-  styleUrl: './products-dashboard.component.css'
+  styleUrl: './products-dashboard.component.css',
 })
-export class ProductsDashboardComponent {
+export class ProductsDashboardComponent implements OnInit {
+  private readonly _ProductsServices = inject(ProductsService);
+  productsList: any[] = [];
 
+  ngOnInit(): void {
+    this.fetchAllProducts();
+  }
+  fetchAllProducts() {
+    this._ProductsServices.getAllProducts().subscribe({
+      next: (res: any) => {
+        this.productsList = res.data;
+        console.log(res.data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
