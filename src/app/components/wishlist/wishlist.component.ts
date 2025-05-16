a05660c5ac5dcbbec96031aadcabfd38d0f8c056
Doooaa <d98744330@gmail.com>
2025-05-16 10:18:52 +0300

    fix some errors add navigation in fav

--- src/app/components/wishlist/wishlist.component.ts
+++ src/app/components/wishlist/wishlist.component.ts
@@ -3,10 +3,12 @@ import { Component, OnInit } from '@angular/core';
 import { WishlistService } from '../../../services/wishlist.service';
 import { ToastService } from '../../../services/toast.service';
 import { BreadcrumbComponent } from '../sharedComponents/breadcrumb/breadcrumb.component';
+import { Router } from '@angular/router';
+import { CartService } from '../../../services/cart.service';
 
 @Component({
   selector: 'app-wishlist',
-  imports: [CommonModule ,BreadcrumbComponent],
+  imports: [CommonModule, BreadcrumbComponent],
   templateUrl: './wishlist.component.html',
   styles: ``,
 })
@@ -15,7 +17,9 @@ export class WishlistComponent implements OnInit {
 
   constructor(
     private _WishlistService: WishlistService,
-    private _ToastService: ToastService
+    private _ToastService: ToastService,
+    private router: Router,
+    private cartService: CartService
   ) {}
 
   ngOnInit(): void {
@@ -33,7 +37,6 @@ export class WishlistComponent implements OnInit {
       },
     });
   }
-  
 
   deleteFromWishlist(id: string) {
     this._WishlistService.deleteFromWishlist(id).subscribe({
@@ -46,5 +49,23 @@ export class WishlistComponent implements OnInit {
         console.error('Failed to del from wishlist', e);
       },
     });
+  }
+
+  addToCart(id: string) {
+    if (localStorage.getItem('user')) {
+      // const fullUrl = this.router.url;
+      // let id = fullUrl.split('/')[2];
+      this.cartService.addToCart({ productId: id }).subscribe({
+        next: (res: any) => {
+          this.cartService.setCartItems(res.data);
+          this._ToastService.show('success', 'Product added to cart!');
+        },
+        error: (err) => {
+          console.log(err.error.message);
+        },
+      });
+    } else {
+      this._ToastService.show('error', 'Login to add items to cart!');
+    }
   }
 }
