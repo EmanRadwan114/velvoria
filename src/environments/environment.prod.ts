7ab3fe31a5ef1dcefe96412ae1b1059210616fe5
Eman Radwan <135042372+EmanRadwan114@users.noreply.github.com>
2026-07-20 16:40:29 +0300

    Update production backend URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backUrl: 'https://velvoria-production.up.railway.app',
+  backUrl: 'https://velvoria-backend-production.up.railway.app',
   stripe_public_key:
     'pk_test_51Pol9VCryGBU1R1YEREtkRC7n63011qOHA2xDeZ9KJBA6BrYtogHPMCVf4ChmvEPxH0lAYO1n0Dfj6eqel44XViP00YwB7TCxD',
 };
